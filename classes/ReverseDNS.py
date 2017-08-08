4f1b6ea1e6d287b0985fedf8526873b53ec3f2d1
spgreen <simonpetergreen@singaren.net.sg>
2017-08-08 11:07:55 +0800

    Added comments and refactored ip variable to ip_address

--- classes/ReverseDNS.py
+++ classes/ReverseDNS.py
@@ -3,41 +3,49 @@ import socket
 
 
 class ReverseDNS:
+    """
+        Performs reverse DNS Lookups on valid IP addresses and stores the IP address
+    and its domain name within the data store. If the IP address has no domain name,
+    the IP address will be stored with itself. Assumption is if an IP address does not
+    have a domain, then it will be unlikely that it will in the future.
+    """
     def __init__(self):
         self.rdns_store = dict()
 
     def query(self, *ip_addresses):
         """
-
-        :param ip_addresses:
-        :return:
+            Performs a reverse DNS lookup on IP addresses by first looking through the
+        rdns_store dictionary. If nothing is found within the said dictionary it will
+        then query from the DNS server.
+        :param ip_addresses: IP Address to be queried
+        :return: list; domain names of said IP addresses
         """
         ip_store = []
-        for ip in ip_addresses:
+        for ip_address in ip_addresses:
             try:
-                ipaddress.ip_address(ip)
-                ip_store.append(self.rdns_store[ip])
+                ipaddress.ip_address(ip_address)
+                ip_store.append(self.rdns_store[ip_address])
             except ValueError:
-                print("%s not a valid IP Address" % ip)
-                ip_store.append(ip)
+                print("Error: %s not a valid IP Address" % ip_address)
+                ip_store.append(ip_address)
             except KeyError:
-                self.rdns_store[ip] = self.__query_from_dns(ip)
-                ip_store.append(self.rdns_store[ip])
+                self.rdns_store[ip_address] = self.__query_from_dns(ip_address)
+                ip_store.append(self.rdns_store[ip_address])
         if len(ip_store) == 1:
             return ip_store[0]
         return ip_store
 
     @staticmethod
-    def __query_from_dns(ip):
+    def __query_from_dns(ip_address):
         """
-
-        :param ip:
-        :return:
+            Queries the local DNS server for the domain name of the IP address
+        :param ip_address: IP Address to be queried
+        :return: Domain name or IP address depending if the lookup was successful
         """
         try:
-            return socket.gethostbyaddr(ip)[0]
+            return socket.gethostbyaddr(ip_address)[0]
         except socket.gaierror:
-            return ip
+            return ip_address
         except socket.herror:
-            print("Unknown Host: %s" % ip)
-            return ip
+            print("Unknown Host: %s" % ip_address)
+            return ip_address
