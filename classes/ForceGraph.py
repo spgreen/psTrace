4c252882216f0dc8428061cedbfaa4c943e025d1
spgreen <simonpetergreen@singaren.net.sg>
2017-08-08 09:53:56 +0800

    Added comments and changed how the force graph dict is generated
    ForceGraph.py
    - Added comments to describe the purpose of said class
    - Added error message if hop_details and previous_hop parameters
      are of different length
    
    - Refactored how the force graph hop dict is generated. Uses enumerate
      instead of relying on the length of the hop_details parameter

--- classes/ForceGraph.py
+++ classes/ForceGraph.py
@@ -1,7 +1,50 @@
 class ForceGraph:
+    """
+    Creates the data required for the D3.js Force Graph used within  the matrix.html.j2 template.
+    It saves each hop and its information as a dict which is then appended to the force graph list.
+    Force graph list example:
+        [
+            {
+                "target": "et-1-0-0.singaren.net.sg",
+                "source": "owamp.singaren.net.sg",
+                "node_point": "source",
+                "type": "okay"
+            },
+            {
+                "target": "sg-mx60.jp.apan.net",
+                "source": "et-1-0-0.singaren.net.sg",
+                "node_point": "",
+                "type": "okay"
+            },
+            {
+                "target": "hk-mx60.jp.apan.net",
+                "source": "sg-mx60.jp.apan.net",
+                "node_point": "",
+                "type": "okay"
+            },
+            {
+                "target": "cuhk.hkix.net",
+                "source": "hk-mx60.jp.apan.net",
+                "node_point": "",
+                "type": "okay"
+            },
+            {
+                "target": "et-0-2-1-cuhk.hkix.net",
+                "source": "cuhk.hkix.net",
+                "node_point": "",
+                "type": "okay"
+            },
+            {
+                "target": "ps1.cuhk.edu.hk",
+                "source": "et-0-2-1-cuhk.hkix.net",
+                "node_point": "destination",
+                "type": "okay"
+            }
+        ]
+    """
     def __init__(self):
         """
-        Initialises the list for storing force graph dictionaries used for D3.js 
+        Initialises the list for storing force graph dictionaries used for D3.js
         found within the matrix.html.j2 template.
         """
         self.force_graph = []
@@ -9,13 +52,22 @@ class ForceGraph:
     def create_force_nodes(self, hop_details, previous_hop, destination_ip):
         """
         Creates the force node dictionary entry that will be appended to the force graph list.
-        :param hop_details: Nested dictionary in list or single dictionary of trace route hop information 
+        Example dictionary:
+            {
+                "target": "et-1-0-0.singaren.net.sg",
+                "source": "owamp.singaren.net.sg",
+                "node_point": "source",
+                "type": "okay"
+            }
+
+        :param hop_details: Nested dictionary in list or single dict of trace route hop information
         :type hop_details: list or dict
-        :param previous_hop: Singular entry or list. If list it will be the route starting at the source ip
+        :param previous_hop: Singular entry or list. If list it will be the route starting
+                             at the source ip
         :type previous_hop: list or str
         :param destination_ip: Trace route destination IP address
         :type destination_ip: str
-        :return: 
+        :return: None
         """
         if not isinstance(hop_details, list):
             hop_details = [hop_details]
@@ -23,22 +75,24 @@ class ForceGraph:
             previous_hop = [previous_hop]
 
         if len(hop_details) != len(previous_hop):
+            print("Error: Hop information and previous hop list are of different length!")
             return
 
-        for i in range(len(hop_details)):
+        for index, hop in enumerate(hop_details):
             node_point = ""
-            if hop_details[i]["ip"] == destination_ip:
+            if hop["ip"] == destination_ip:
                 node_point = "destination"
-            elif i == 0:
+            elif index == 0:
                 node_point = "source"
 
-            self.force_graph.append({"source": previous_hop[i],
-                                     "target": hop_details[i]["domain"],
-                                     "type": hop_details[i]["status"],
+            self.force_graph.append({"source": previous_hop[index],
+                                     "target": hop["domain"],
+                                     "type": hop["status"],
                                      "node_point": node_point})
 
     def retrieve_graph(self):
         """
-        :return: dict; current force graph list state 
+        Returns the current force graph state
+        :return: list
         """
         return self.force_graph
